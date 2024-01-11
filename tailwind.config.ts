a94658a1bf94199b858b3c9792a169e0440f1bce
samprit.c <samprit.c@media.net>
2024-01-11 20:35:47 +0530

    Add static UI for Home, Cart, Category & Product

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        customWhite: '#FFFFFF',
+        customBlue: '#4CA9EE',
+        customDarkGreen: '#238878',
+        customLightGreen: '#5ECD81',
+        customGray: '#B2B7BB',
+      },
     },
   },
   plugins: [],
